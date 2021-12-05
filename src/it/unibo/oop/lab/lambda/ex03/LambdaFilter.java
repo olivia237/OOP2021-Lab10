2eb0e93007d300313265a378edb2b6d5132c1214
olivia nguemo <oliviarannick.nguemo@studio.unibo.it>
2021-12-05 02:55:32 +0100

    ultimo punto da rifare

--- src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
+++ src/it/unibo/oop/lab/lambda/ex03/LambdaFilter.java
@@ -7,6 +7,7 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -35,8 +36,24 @@ public final class LambdaFilter extends JFrame {
     private static final long serialVersionUID = 1760990730218643730L;
 
     private enum Command {
-        IDENTITY("No modifications", Function.identity());
-
+        IDENTITY("No modifications", Function.identity()),
+    	LOWERCASE("ToLowercase", (e -> e.toLowerCase())),
+    	NUMBER_OF_CHARS("CountChars", (e -> Integer.toString(e.length()))),
+        NUMBER_OF_LINES("CountLines", (i->Long.toString(i.chars().filter(e-> e == '\n').count()+1)),
+    	ALPHABETICAL_ORDER("WordsInAlfabeticalOrder",(e->Arrays.stream(e.split("(\\s|\\p{Punct})+"))
+    															.sorted()
+    															.collect(Collectors.joining("\n")));
+    	
+    	 WORDCOUNT("Count words", s ->
+         Arrays.stream(s.split(ANY_NON_WORD))
+             .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+             .entrySet().stream()
+             .map(e -> e.getKey() + " -> " + e.getValue())
+             .collect(Collectors.joining("\n"))
+     );
+    	
+    	
+    	
         private final String commandName;
         private final Function<String, String> fun;
 
